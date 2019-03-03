b63b4f86e32369954425fd5ebfbdc8c0794f2ea9
Darpan Beri <darpanberi.99@gmail.com>
2019-03-03 22:22:31 +0000

    First iteration of Psuedocode

--- xed.cpp
+++ xed.cpp
@@ -4,6 +4,18 @@
             a. Check for actual argument
             b. check for existence for .obj and .sym file
         1.b If check fail, do graceful exit
+
+        2. Initalize .sic and .lis files. Maybe make one more intermediatory file for debugging purposes.
+            a. Create file via C++.
+
+        3. Algorithm for disassembling
+            a. Read the .obj and .sym file.
+            b. Interpret the contents via code.
+            c. Write line by line? or store in intermediatory file and then write to .lis and .sic.
+
+        3.5  Writing it to files
+            d. Write the .lis file
+            e. Write the .sic file
 */
 
 
@@ -16,4 +28,14 @@
 
     % xed <filename>
     
+*/
+
+/*
+    Tips from prof:
+
+    Graceful Exit:
+        exit() -> Test return value on every system call. Int error handling routine
+    
+    Don't use templates in C++.
+    Use C versions of I/O commands
 */
