a859e1e3398d1fb853e51d5eb76148e38cc4ff3d
Darpan Beri <darpanberi.99@gmail.com>
2019-03-03 21:53:47 +0000

    Added Psuedocode

---
+++ xed.cpp
@@ -0,0 +1,19 @@
+/*
+    Pseduo code as of now:
+        1. We check for correct input:
+            a. Check for actual argument
+            b. check for existence for .obj and .sym file
+        1.b If check fail, do graceful exit
+*/
+
+
+// Make Graceful exit function which includes tailored exit messages
+
+/*
+    Input                   Output
+    <filename>.obj           <filename>.sic
+    <filename>.sym           <filename>.lis
+
+    % xed <filename>
+    
+*/
