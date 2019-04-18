5ec39f509f9b87258bb94d94a29555a22f976d17
Zach Selchau <zhselchau1998@yahoo.com>
2019-04-18 23:53:50 +0000

    removed substr from end line logic

--- xed.cpp
+++ xed.cpp
@@ -1328,10 +1328,10 @@ void writeSicFile(FILE *fp, vector<string> objVector, Symbol *symHead, Literal *
     }
 
     //End line
-    tmpSym = findAddressInSymtab(symHead, objVector[objVector.size()-1].substr(1,6));
+    tmpSym = findAddressInSymtab(symHead, objVector[objVector.size()-1]);
 
     if(tmpSym != nullptr) fprintf(fp, "         END     %s", tmpSym->getName().c_str());
-    else fprintf(fp, "         END   %s", objVector[objVector.size()-1].substr(1,6).c_str());
+    else fprintf(fp, "         END   %s", objVector[objVector.size()-1].c_str());
     fputc(10, fp);
     
 
